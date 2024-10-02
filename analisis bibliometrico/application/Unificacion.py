23f1e3156c759c68c747832a29f69e7c60ac2bd0
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-10-02 12:11:47 -0500

    se organizo el codigo de mejor manera y se adapto a los datos reales para la unificacion

--- analisis bibliometrico/application/Unificacion.py
+++ analisis bibliometrico/application/Unificacion.py
@@ -3,11 +3,11 @@ import os
 
 
 ##variables globales
-data_IEEE=[]
-data_scopus=[]
+data_IEEE=None
+data_scopus=None
 
 #dataframe unificado 
-datra=[]
+data=None
 
 
 #Metodo que carga los datos de la base scopus y unifica las columnas 
@@ -22,9 +22,13 @@ def cargarDatosScopus():
                        on_bad_lines='skip',
                        encoding_errors='replace')
 
+    #columnas especificas a extraer de la data de scopus
     columnas_deseadas=['Authors', 'Title', 'Year', 'Issue', 'Page start', 'Page end', 'Abstract', 'Affiliations', 'ISBN', 'DOI', 'Link', 'EID', 'Publisher']    
 
     data_scopus=data[columnas_deseadas]
+
+
+
 
 
 #Metodo que carga los datos de la base IEEE y unifica las columnas 
@@ -32,22 +36,70 @@ def cargarDatosIEEE():
 
     global data_IEEE
 
+    #extraer direccion del archivo
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     file_path = os.path.join(base_dir, 'data', 'dataIEEE.csv')
 
+    #leer el archivo
     data = pd.read_csv(file_path, nrows=10150, encoding='utf-8',
                        on_bad_lines='skip',
                        encoding_errors='replace')
     
-    columnas_deseadas = ['Authors', 'Publication Title', 'Publication Year', 'Issue', 'Start Page', 'End Page', 'Abstract', 'Author Affiliations', 'ISBNs', 'DOI', 'PDF Link', 'Document Identifier', 'Publisher']  # Reemplaza con los nombres de las columnas que deseas extraer
+    #columnas especificas a extraer de la data de IEEE
+    columnas_deseadas = ['Authors', 'Publication Title', 'Publication Year', 'Issue', 'Start Page', 'End Page', 'Abstract', 'Author Affiliations', 'ISBNs', 'DOI', 'PDF Link', 'Document Identifier', 'Publisher']  
 
     data_IEEE=data[columnas_deseadas]
-    
-    
 
     
 
 
 
 
+    
+#metodo encargado de renombrar las columnas de la base de IEEE y unificarla a la de scopus
+def unificarDatos():
 
+    global data_IEEE,data,data_scopus
+
+    data_IEEE=data_IEEE.rename(columns={
+        'Publication Title':'Title',
+        'Publication Year':'Year',
+        'Start Page':'Page start',
+        'End Page':'Page end',
+        'Author Affiliations':'Affiliations',
+        'ISBNs':'ISBN',
+        'PDF Link':'Link',
+        'Document Identifier':'EID'
+
+    })
+
+    data = pd.concat([data_IEEE,data_scopus],ignore_index=True)
+
+    print(data.iloc[0])
+    
+
+
+#Metodo encargado de generar el nuevo archivo con todos los datos unificados pero sin repetir
+def crearNuevoCsvSinDuplicados():
+    
+    global data,data_IEEE
+
+    #se eliminan los duplicados de la data
+    data=data.drop_duplicates(subset=['DOI'])
+
+    #extraer la direccion donde sera guardado el archivo
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    direccion = os.path.join(base_dir, 'data', 'data.csv')
+
+    data.to_csv(direccion,index=False)
+
+
+
+
+
+
+
+cargarDatosIEEE()
+cargarDatosScopus()
+unificarDatos()
+crearNuevoCsvSinDuplicados()
