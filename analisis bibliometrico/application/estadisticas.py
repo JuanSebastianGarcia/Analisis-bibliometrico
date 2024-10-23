0756fc11d2e587611fff36184b60cd4c18e7af57
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-10-23 15:53:00 -0500

    se agrego el proceso estadistico que analiza la cantidad de veces que aparece un autor al inicio de un articulo y muestra una grafica

--- analisis bibliometrico/application/estadisticas.py
+++ analisis bibliometrico/application/estadisticas.py
@@ -15,24 +15,66 @@ Lista de las variables que hay que procesar
 #IMPORTS
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 #VARIABLES GLOBALES
 data = None 
 
-def AnalizarAutores():
+def analizarAutores():
 
     """
         Se extrae el primer autor de cada producto y se hace un contedo de ellos
         para poder mostrar un resultado
 
     """
-    #extraer lista de autores
-    print()
+    global data
+
+    #extraer el primer autor    
+    autores = data['Authors'].apply(lambda x: x.split(',')[0].split(';')[0].strip())
+
+    counting_autores={}
+
+    #hacer un conteo de los autores
+    for autor in autores:
+        if autor in counting_autores:
+            counting_autores[autor]+=1
+        else:
+            counting_autores[autor]=1
+
+    imprimirGraficaautores(counting_autores)
+
+
+def imprimirGraficaautores(counting_autores):
+    """
+        Deacuerdo al conteo de los autores hecho previamente, se imprimira una grafica de barras con los autores que tengan 
+        mas de 5 apariciones
+    """
+
+    # Filtrar autores con más de 5 ocurrencias
+    autores_filtrados = {autor: conteo for autor, conteo in counting_autores.items() if conteo >= 5}
+
+    keys = autores_filtrados.keys()
+    values = autores_filtrados.values()
+
+    plt.bar(keys,values)
+
+    # Añadir etiquetas
+    plt.xlabel('Autores')
+    plt.ylabel('Número de publicaciones')
+    plt.title('Conteo de autores (más de 5 publicaciones)')
+
+    # Mostrar el gráfico
+    plt.xticks(rotation=90)  # Rotar etiquetas si son largas
+    plt.show()
+
+
+
     
 
 
 def cargarDatos():
+
     """
         Se lee el archivo csv con la data y se almacena en una variable
     """
@@ -46,3 +88,18 @@ def cargarDatos():
     data = pd.read_csv(file_path, nrows=10150, encoding='utf-8',
                        on_bad_lines='skip',
                        encoding_errors='replace')
+    
+
+def estandarizarTiposDatos():
+    """
+        Para evitar errores en la lectura de los datos y confundir tipos, se estandarizan los 
+        tipos de datos desde el inicio
+    """
+    global data
+
+    data['Authors'] = data['Authors'].astype(str).fillna('')
+
+
+cargarDatos()
+estandarizarTiposDatos()
+analizarAutores()
