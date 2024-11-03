0039ce77c55ec89186f9ae170b6ec93764c1e51c
Juan Sebastian Garcia Alzate <117672609+JuanSebastianGarcia@users.noreply.github.com>
2024-11-03 17:26:03 -0500

    se establecio el orden de ejecucion que debe llevar la clase Frecuencia para poder dar los resultados esperados

--- analisis bibliometrico/application/Frecuencia.py
+++ analisis bibliometrico/application/Frecuencia.py
@@ -1,17 +1,15 @@
-import pandas as pd
-import os
+class Frecuencia:
 
-base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-file_path = os.path.join(base_dir, 'data', 'Palabras_especiales.csv')
+    #metodo constructor
+    def __init__(self):
+        pass
 
-#cargar archivo
-data = pd.read_csv(file_path,nrows=500,encoding='utf-8',
-                       on_bad_lines='skip',
-                       encoding_errors='replace')
+    def iniciarEjecucion():
 
-#recorrer registro por registro
-data['Sinonimos'] = data['Palabra'].apply(lambda x: str(x).split(' - ')[1:] if ' - ' in str(x) else [])
-data['Palabra'] = data['Palabra'].apply(lambda x: str(x).split(' - ')[0])
+        #paso 1: estandarizar palabras, analizar y remplazar todos los sinonimos a una sola palabra
 
-#extraer los sinonimos de la columna palabra y colocarlos en una nueva columna
-data.to_csv(file_path,index=False)
+        #paso 2: hacer la matriz de frecuencia con las palabras especificas que queremos usar
+
+        #imprimir resultados en una tabla 
+
+        #generar una nube de palabras
